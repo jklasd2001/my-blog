25cc4fe97efdb3296035294b3e9a7ee9148b614d
jklasd2001 <jklasd2001@naver.com>
2022-11-04 09:05:13 +0900

    feat: package.json sort

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,9 +1,9 @@
-// import { Fragment } from 'react'
+import { Fragment } from 'react'
 
-// import type { GatsbyBrowser } from 'gatsby'
+import type { GatsbyBrowser } from 'gatsby'
 
 import './src/styles/global.css'
 
-// export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({ element }) => {
-//   return <Fragment />
-// }
+export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({ element }) => {
+  return <Fragment>{element}</Fragment>
+}
