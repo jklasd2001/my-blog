8e2c44ab97a39c86210d14d5e82733cf23efadac
김영수 주임 연구원 <jklasd2001@musma.net>
2022-11-26 17:06:13 +0900

    feat: Tailwind content src/template 하위 파일 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}', './src/templates/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
